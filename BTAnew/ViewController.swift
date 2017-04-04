b99f18bb2b299e7a3964bf0c4940e860d53349f1
Ingridoshka <Ingridoshka@MacBooks-MacBook-Pro.local>
2017-04-04 20:14:22 +0300

    Initial Commit

---
+++ BTAnew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BTAnew
+//
+//  Created by Ingridoshka on 2017-04-04.
+//  Copyright © 2017 MacBook Retina 2014 256Gb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
